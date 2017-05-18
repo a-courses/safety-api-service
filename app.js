35611288e7420cb2263495577d0596fb3c9d298f
Sandeep <j.sandeepm@gmail.com>
2017-05-18 22:33:07 +0530

    first commit: node api

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.text({type: '*/*'}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+var router = express.Router();
+router.get('/getAlertMessages', function(req, res) {
+    console.log(req.query);
+    console.log(req.body);
+    res.send(JSON.stringify(["item1", "item2", "item3"]));
+});
+router.post("/writeAlertMessages", function (req, res) {
+    req.setEncoding('utf8');
+    console.log(req.body);
+    res.send(req.body);
+
+});
+app.use('/api', router);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    console.log(err)
+});
+
+module.exports = app;
