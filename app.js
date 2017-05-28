c6753a1daaca7d751d5fed1e4c9db598376c840a
Sandeep <j.sandeepm@gmail.com>
2017-05-28 19:25:40 +0530

    Writting messages to DeepStream server

--- app.js
+++ app.js
@@ -41,61 +41,49 @@ router.post("/writeAlertMessages", function (req, res) {
     console.log("---------------------------------------");
     console.log(req.body);
     console.log("---------------------------------------");
-    var alertMessages = {
-        Type: "Notification",
-        MessageId: "f9a037e9-389f-5eb2-8749-664dbad67398",
-        TopicArn: "arn:aws:sns:us-west-2:421971994929:FileAlertTopic",
-        Message: {
-            id: "9bdb3690-7d97-4b4d-a861-6785786c69a3",
-            url: "https://esbucketservice.s3.amazonaws.com/from-object-Test.pdf",
-            fileName: "from-object-Test.pdf",
-            user: {
-                phoneNumber: "7847851785",
-                emailId: "manmohan_saini82@rediffmail.com",
-                userName: "msaini"
-            },
-            location: {
-                latitude: 14,
-                longitude: 12
-            },
-            status: "read",
-            mediaType: "jpeg",
-            incidentType: "Fire",
-            time: "2017/05/25 06:50:04",
-            incidentId: "12345"
-        },
-        Timestamp: "2018-05-28 07:45:13.261",
-        SignatureVersion: "1",
-        Signature: "MUAEZcOqI4cRpn73Mc5kwAAr8KFJPKuoAKbI1HU8BvwwWBY5tv8WTtlmJmKU9lCyfuX5LAh5XWpF66aTA0ONKqqOAKzoKEMI9uuswOv1w0ZWHmQqDQSASRTXiW9zjyxFDHwOgkaCrSmz8978R9B1AsaPdKZpFvjU2NQasqvU1owghGG/yDmkC7BDyuOswdUfwg+5obQQQHFLFrKpe++e7wjj9LVdfL8475z6IjJhVDjza9V8t51D+iEShbo8+Dl+WOjLBwBjC+cM+rHfHai89Y/S9h8PC2O7qpCGUoN2Ey52Hrrai+5LJKUIaw1S6DoEI4ViJ6qjpKMqyjN5Tk7QTA==",
-        SigningCertURL: "https://sns.us-west-2.amazonaws.com/SimpleNotificationService-b95095beb82e8f6a046b3aafc7f4149a.pem",
-        UnsubscribeURL: "https://sns.us-west-2.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-west-2:421971994929:FileAlertTopic:3ab86d28-daa4-41d8-9824-eb60a2c39bf9"
+    var alertMessages =
+    {
+        "Type": "Notification",
+        "MessageId": "a9ff8a52-feff-5656-b21a-dbcd4aba3ffb",
+        "TopicArn": "arn:aws:sns:us-west-2:421971994929:FileAlertTopic",
+        "Message": "{\"id\":\"5dc7dbae-1228-4dc2-97aa-bd387b6abfbb\",\"caller\":{\"phoneNumber\":\"8800094877\",\"emailId\":\"manmohan_saini82@rediffmail.com\",\"userName\":\"msaini\"},\"callee\":{\"phoneNumber\":\"8800094877\",\"emailId\":\"manmohan_saini82@rediffmail.com\",\"userName\":\"msaini\"},\"location\":{\"latitude\":14.0,\"longitude\":12.0},\"status\":\"new\",\"mediaType\":\"jpeg\",\"incidentType\":\"Fire\",\"time\":\"2017/05/12 16:38:48\",\"incidentId\":\"12345\"}",
+        "Timestamp": "2017-05-24T16:17:09.732Z",
+        "SignatureVersion": "1",
+        "Signature": "KHdQr/jyyLZ6nt9p/BUTfIdfEpKXzQudBUHP9gbTbHD8euemE0UpfR70+GMHPzmybShynZf2ntY57ex4ZgrGi8nC7W5gyzbFJn/kHDrYEpJNgmtAufFrHFGjZRO+J0Ca7bAdrmWaZvDimbO9ZTrfMmYAO4R3EYBuXJuw8MVfi+yEx42pgLn1iUURiPTfP1TRHWUhSjquU91dhc/8UBRia/3bC+xMWOPS61GEbxHON8uxs6yQdiGbZE8hWO05HXegtnC/lzKJoon4LwKYZ7TL13+ukZGViR3ggzewc+ga4A/zMTxLMyJ73fN32uWsYy9cUYdtzNOkgRLYABRbp2MJhQ==",
+        "SigningCertURL": "https://sns.us-west-2.amazonaws.com/SimpleNotificationService-b95095beb82e8f6a046b3aafc7f4149a.pem",
+        "UnsubscribeURL": "https://sns.us-west-2.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-west-2:421971994929:FileAlertTopic:9118ae30-a6b0-4bc9-8c42-5c1d2796f421"
     };
 
     if (req.body !== undefined && req.body.Type === 'Notification') {
+        var messageBody = JSON.parse(req.body.Message);
+        console.log("---------------------Alert Message------------------------");
+        console.log(messageBody);
+        console.log("----------------------------------------------------------");
         var client = connectDeepStream();
         var recordList = client.record.getList("safety/alerts");
         var name = 'alerts/' + client.getUid();
         var newRecordName = client.record.getRecord(name);
         newRecordName.set({
-            id: req.body.Message.id,
-            url: req.body.Message.url,
-            fileName: req.body.Message.fileName,
+            id: messageBody.id,
+            url: messageBody.url,
+            fileName: messageBody.fileName,
             user: {
-                phoneNumber: req.body.Message.user.phoneNumber,
-                emailId: req.body.Message.user.emailId,
-                userName: req.body.Message.user.userName
+                phoneNumber: messageBody.user.phoneNumber,
+                emailId: messageBody.user.emailId,
+                userName: messageBody.user.userName
             },
             location: {
-                latitude: req.body.Message.location.latitude,
-                longitude: req.body.Message.location.longitude
+                latitude: messageBody.location.latitude,
+                longitude: messageBody.location.longitude
             },
-            status: req.body.Message.status,
-            mediaType: req.body.Message.mediaType,
-            incidentType: req.body.Message.incidentType,
-            time: req.body.Message.time,
-            incidentId: req.body.Message.incidentId
+            status: messageBody.status,
+            mediaType: messageBody.mediaType,
+            incidentType: messageBody.incidentType,
+            time: messageBody.time,
+            incidentId: messageBody.incidentId
         });
         recordList.addEntry(name);
+        console.log("--------[New record published to deepstream]--------");
         res.send("success");
     } else {
         res.send("request is invalid");
@@ -118,7 +106,9 @@ app.use(function (err, req, res, next) {
 
     // render the error page
     res.status(err.status || 500);
+    console.log("-----------Error--------------");
     console.log(err)
+    console.log("------------------------------");
 });
 
 module.exports = app;
