774f07e5892c3f50cf311ed6da9092f09997122d
Sandeep <j.sandeepm@gmail.com>
2017-06-04 12:06:11 +0530

    Writting messages to DeepStream server for call/stream/incident

--- app.js
+++ app.js
@@ -38,147 +38,181 @@ router.get('/getAlertMessages', function (req, res) {
 });
 
 router.post("/writeAlertMessages", function (req, res) {
-    req.setEncoding('utf8');
-    console.log("---------------------------------------");
-    var requestType = req.get('Content-Type');
-    console.log(requestType);
+        req.setEncoding('utf8');
+        console.log("---------------------------------------");
+        var requestType = req.get('Content-Type');
+        console.log(requestType);
 
-    req.body = JSON.parse(req.body);
-    console.log(req.body);
-    console.log(req.body.Type === 'Notification');
-    console.log("---------------------------------------");
+        req.body = JSON.parse(req.body);
+        console.log(req.body);
+        console.log(req.body.Type === 'Notification');
+        console.log("---------------------------------------");
 
-    if (req.body !== undefined && req.body.Type === 'Notification') {
-        var messageBody = JSON.parse(req.body.Message);
-        console.log("---------------------Alert Message------------------------");
-        console.log(messageBody);
-        console.log("----------------------------------------------------------");
-        var client = connectDeepStream();
-        var recordList = client.record.getList("safety/alerts");
-        if (messageBody.notificationType === 'upload' || messageBody.notificationType === 'stream') {
-            var name = 'alerts/' + client.getUid();
-            var newRecordName = client.record.getRecord(name);
-            console.log("----In create of " + messageBody.notificationType + "-------------: " + name);
-            newRecordName.set({
-                type: messageBody.notificationType,
-                id: messageBody.alert.id,
-                url: messageBody.alert.url,
-                fileName: messageBody.alert.fileName,
-                user: {
-                    phoneNumber: messageBody.alert.user.phoneNumber,
-                    emailId: messageBody.alert.user.emailId,
-                    userName: messageBody.alert.user.userName
-                },
-                location: {
-                    latitude: messageBody.alert.location.latitude,
-                    longitude: messageBody.alert.location.longitude
-                },
-                status: messageBody.alert.status,
-                mediaType: messageBody.alert.mediaType,
-                incidentType: messageBody.alert.incidentType,
-                time: messageBody.alert.time,
-                incidentId: messageBody.alert.incidentId
-            });
-            recordList.addEntry(name);
-            console.log("--------[New upload alert published to deepstream]--------");
-            res.send("SUCCESS : New '" + messageBody.notificationType.toUpperCase() + "' alert published to deepstream");
-        }
-        if (messageBody.notificationType === 'call') {
-            var name = 'alerts/' + client.getUid();
-            var newRecordName = client.record.getRecord(name);
-            console.log("----In create of " + messageBody.notificationType + "-------------:" + name);
-            newRecordName.set({
-                type: messageBody.notificationType,
-                id: messageBody.alert.id,
-                caller: {
-                    phoneNumber: messageBody.alert.caller.phoneNumber,
-                    emailId: messageBody.alert.caller.emailId,
-                    userName: messageBody.alert.caller.userName
-                },
-                callee: {
-                    phoneNumber: messageBody.alert.callee.phoneNumber,
-                    emailId: messageBody.alert.callee.emailId,
-                    userName: messageBody.alert.callee.userName
-                },
-                location: {
-                    latitude: messageBody.alert.location.latitude,
-                    longitude: messageBody.alert.location.longitude
-                },
-                status: messageBody.alert.status,
-                mediaType: messageBody.alert.mediaType,
-                incidentType: messageBody.alert.incidentType,
-                time: messageBody.alert.time,
-                incidentId: messageBody.alert.incidentId
-            });
-            recordList.addEntry(name);
-            console.log("--------[New call alert published to deepstream]--------");
-            res.send("SUCCESS : New '" + messageBody.notificationType.toUpperCase() + "' alert published to deepstream");
-        }
-        if (messageBody.notificationType === 'incident') {
-            console.log("----In create of " + messageBody.notificationType + "-------------");
-            var name = 'alerts/' + client.getUid();
-            var newRecordName = client.record.getRecord(name);
-            console.log("----In create of " + messageBody.notificationType + "-------------:" + name);
-            newRecordName.set({
-                notificationType: messageBody.notificationType,
-                alert: {
+        if (req.body !== undefined && req.body.Type === 'Notification') {
+            var messageBody = JSON.parse(req.body.Message);
+            console.log("---------------------Alert Message------------------------");
+            console.log(messageBody);
+            console.log("----------------------------------------------------------");
+            var client = connectDeepStream();
+            var recordList = client.record.getList("safety/alerts");
+            if (messageBody.notificationType === 'upload' || messageBody.notificationType === 'stream') {
+                var name = 'alerts/' + client.getUid();
+                var newRecordName = client.record.getRecord(name);
+                console.log("----In create of " + messageBody.notificationType + "-------------: " + name);
+                newRecordName.set({
+                    type: messageBody.notificationType,
                     id: messageBody.alert.id,
-                    time: messageBody.alert.time,
-                    name: messageBody.alert.name,
-                    parentAlert: [
-                        {
-                            id: messageBody.alert.parentAlert.id,
-                            caller: {
-                                phone: messageBody.alert.parentAlert.caller.phone,
-                                emailId: messageBody.alert.parentAlert.caller.emailId,
-                                userName: messageBody.alert.parentAlert.caller.userName
-                            },
-                            callee: {
-                                phone: messageBody.alert.parentAlert.callee.phone,
-                                emailId: messageBody.alert.parentAlert.callee.emailId,
-                                userName: messageBody.alert.parentAlert.callee.userName
-                            },
-                            location: {
-                                latitude: messageBody.alert.parentAlert.latitude,
-                                longitude: messageBody.alert.parentAlert.longitude
-                            },
-                            status: messageBody.alert.parentAlert.status,
-                            mediaType: messageBody.alert.parentAlert.mediaType,
-                            incidentType: messageBody.alert.parentAlert.incidentType,
-                            time: messageBody.alert.parentAlert.time,
-                            incidentId: messageBody.alert.parentAlert.incidentId
-                        }
-                    ],
-                    mappedAlerts: messageBody.alert.mappedAlerts,
-                    createdBy: messageBody.alert.createdBy,
-                    msadescription: messageBody.alert.msadescription,
+                    url: messageBody.alert.url,
+                    fileName: messageBody.alert.fileName,
+                    user: {
+                        phoneNumber: messageBody.alert.user.phoneNumber,
+                        emailId: messageBody.alert.user.emailId,
+                        userName: messageBody.alert.user.userName
+                    },
+                    location: {
+                        latitude: messageBody.alert.location.latitude,
+                        longitude: messageBody.alert.location.longitude
+                    },
                     status: messageBody.alert.status,
-                    assignedTo: [
-                        {
-                            phone: messageBody.alert.assignedTo.phone,
-                            emailId: messageBody.alert.assignedTo.emailId,
-                            userName: messageBody.alert.assignedTo.userName
+                    mediaType: messageBody.alert.mediaType,
+                    incidentType: messageBody.alert.incidentType,
+                    time: messageBody.alert.time,
+                    incidentId: messageBody.alert.incidentId
+                });
+                recordList.addEntry(name);
+                console.log("--------[New upload alert published to deepstream]--------");
+                res.send("SUCCESS : New '" + messageBody.notificationType.toUpperCase() + "' alert published to deepstream");
+            }
+            if (messageBody.notificationType === 'call') {
+                var name = 'alerts/' + client.getUid();
+                var newRecordName = client.record.getRecord(name);
+                console.log("----In create of " + messageBody.notificationType + "-------------:" + name);
+                newRecordName.set({
+                    type: messageBody.notificationType,
+                    id: messageBody.alert.id,
+                    caller: {
+                        phoneNumber: messageBody.alert.caller.phoneNumber,
+                        emailId: messageBody.alert.caller.emailId,
+                        userName: messageBody.alert.caller.userName
+                    },
+                    callee: {
+                        phoneNumber: messageBody.alert.callee.phoneNumber,
+                        emailId: messageBody.alert.callee.emailId,
+                        userName: messageBody.alert.callee.userName
+                    },
+                    location: {
+                        latitude: messageBody.alert.location.latitude,
+                        longitude: messageBody.alert.location.longitude
+                    },
+                    status: messageBody.alert.status,
+                    mediaType: messageBody.alert.mediaType,
+                    incidentType: messageBody.alert.incidentType,
+                    time: messageBody.alert.time,
+                    incidentId: messageBody.alert.incidentId
+                });
+                recordList.addEntry(name);
+                console.log("--------[New call alert published to deepstream]--------");
+                res.send("SUCCESS : New '" + messageBody.notificationType.toUpperCase() + "' alert published to deepstream");
+            }
+            if (messageBody.notificationType === 'incident') {
+                console.log("----In create of " + messageBody.notificationType + "-------------");
+                var name = 'alerts/' + client.getUid();
+                var newRecordName = client.record.getRecord(name);
+                console.log("----In create of " + messageBody.notificationType + "-------------:" + name);
+                var parentAlerts = [];
+                var assignedToList = [];
+                var alertUsersList = [];
+                var mappedAlertList = [];
+                for (var i in messageBody.alert.parentAlert) {
+                    var singleAlert = {
+                        id: messageBody.alert.parentAlert[i].id,
+                        caller: {
+                            phone: messageBody.alert.parentAlert[i].caller.phone,
+                            emailId: messageBody.alert.parentAlert[i].caller.emailId,
+                            userName: messageBody.alert.parentAlert[i].caller.userName
+                        },
+                        callee: {
+                            phone: messageBody.alert.parentAlert[i].callee.phoneNumber,
+                            emailId: messageBody.alert.parentAlert[i].callee.emailId,
+                            userName: messageBody.alert.parentAlert[i].callee.userName
+                        },
+                        location: {
+                            latitude: messageBody.alert.parentAlert[i].location.latitude,
+                            longitude: messageBody.alert.parentAlert[i].location.longitude
                         }
-                    ],
-                    alertUsers: [
-                        {
-                            phone: messageBody.alert.alertUsers.phone,
-                            emailId: messageBody.alert.alertUsers.emailId,
-                            userName: messageBody.alert.alertUsers.userName
-                        }
-                    ]
+                    };
+                    parentAlerts.push(singleAlert);
                 }
-            });
-            recordList.addEntry(name);
-            console.log("--------[New incident published to deepstream]--------");
-            res.send("SUCCESS : New '" + messageBody.notificationType.toUpperCase() + "' alert published to deepstream");
+                console.log(parentAlerts.length);
+                for (var i in messageBody.alert.mappedAlerts) {
+                    var singleAlert = {
+                        id: messageBody.alert.mappedAlerts[i].id,
+                        caller: {
+                            phone: messageBody.alert.mappedAlerts[i].caller.phone,
+                            emailId: messageBody.alert.mappedAlerts[i].caller.emailId,
+                            userName: messageBody.alert.mappedAlerts[i].caller.userName
+                        },
+                        callee: {
+                            phone: messageBody.alert.mappedAlerts[i].callee.phoneNumber,
+                            emailId: messageBody.alert.mappedAlerts[i].callee.emailId,
+                            userName: messageBody.alert.mappedAlerts[i].callee.userName
+                        },
+                        location: {
+                            latitude: messageBody.alert.mappedAlerts[i].location.latitude,
+                            longitude: messageBody.alert.mappedAlerts[i].location.longitude
+                        }
+                    };
+                    mappedAlertList.push(singleAlert);
+                }
+                console.log(mappedAlertList.length);
+
+                for (var i in messageBody.alert.assignedTo) {
+                    var assign = {
+                        phone: messageBody.alert.assignedTo[i].phone,
+                        emailId: messageBody.alert.assignedTo[i].emailId,
+                        userName: messageBody.alert.assignedTo[i].userName
+                    };
+                    assignedToList.push(assign);
+                }
+
+                console.log(assignedToList.length);
+
+                for (var i in messageBody.alert.alertUsers) {
+                    var user = {
+                        phone: messageBody.alert.alertUsers[i].phone,
+                        emailId: messageBody.alert.alertUsers[i].emailId,
+                        userName: messageBody.alert.alertUsers[i].userName
+                    };
+                    alertUsersList.push(user);
+                }
+                console.log(alertUsersList.length);
+
+                newRecordName.set({
+                    notificationType: messageBody.notificationType,
+                    alert: {
+                        id: messageBody.alert.id,
+                        time: messageBody.alert.time,
+                        name: messageBody.alert.name,
+                        parentAlert: parentAlerts,
+                        mappedAlerts: messageBody.alert.mappedAlerts,
+                        createdBy: messageBody.alert.createdBy,
+                        msadescription: messageBody.alert.msadescription,
+                        status: messageBody.alert.status,
+                        assignedTo: assignedToList,
+                        alertUsers: alertUsersList
+                    }
+                });
+                recordList.addEntry(name);
+                console.log("--------[New incident published to deepstream]--------");
+                res.send("SUCCESS : New '" + messageBody.notificationType.toUpperCase() + "' alert published to deepstream");
+            }
+        }
+        else {
+            console.log("request is invalid");
+            res.error("request is invalid");
         }
     }
-    else {
-        console.log("request is invalid");
-        res.error("request is invalid");
-    }
-});
+);
 app.use('/api', router);
 
 module.exports = app;
