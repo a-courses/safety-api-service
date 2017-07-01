7be5acb3ff15b39e07d554d0c6fb9122f9aa706e
Sandeep <j.sandeepm@gmail.com>
2017-07-02 00:58:53 +0530

    Writting messages to DeepStream server for call/stream/incident

--- app.js
+++ app.js
@@ -336,9 +336,9 @@ router.post("/writeAlertMessages", function (req, res) {
                                             var singleAlert = {
                                                 id: messageBody.alert.parentAlert[i].id,
                                                 user: {
-                                                    phoneNumber: messageBody.alert.parentAlert[i].caller.phoneNumber,
-                                                    emailId: messageBody.alert.parentAlert[i].caller.emailId,
-                                                    userName: messageBody.alert.parentAlert[i].caller.userName
+                                                    phoneNumber: messageBody.alert.parentAlert[i].user.phoneNumber,
+                                                    emailId: messageBody.alert.parentAlert[i].user.emailId,
+                                                    userName: messageBody.alert.parentAlert[i].user.userName
                                                 },
                                                 location: {
                                                     latitude: messageBody.alert.parentAlert[i].location.latitude,
@@ -372,9 +372,9 @@ router.post("/writeAlertMessages", function (req, res) {
                                             var singleAlert = {
                                                 id: messageBody.alert.parentAlert[i].id,
                                                 user: {
-                                                    phoneNumber: messageBody.alert.parentAlert[i].caller.phoneNumber,
-                                                    emailId: messageBody.alert.parentAlert[i].caller.emailId,
-                                                    userName: messageBody.alert.parentAlert[i].caller.userName
+                                                    phoneNumber: messageBody.alert.parentAlert[i].user.phoneNumber,
+                                                    emailId: messageBody.alert.parentAlert[i].user.emailId,
+                                                    userName: messageBody.alert.parentAlert[i].user.userName
                                                 },
                                                 location: {
                                                     latitude: messageBody.alert.parentAlert[i].location.latitude,
